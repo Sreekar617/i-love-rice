ee6ae039ef3abb478c6c42732468778eb6f0e469
Sreekar617 <115044820+Sreekar617@users.noreply.github.com>
2023-05-03 17:10:26 -0500

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,34 @@
+function start() {
+
+  ////CONFIG////
+
+  var link = "https://www.youtube.com/"; // link to app webpage
+  var title = "Youtube"; // title shown in navbar
+  var id = "https://redstone-nw.netlify.app/clockwork-app/twitter.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  ExampleLink.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    closeApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
